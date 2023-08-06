584ca9ea1a9f885454dcc1322f9a4a90290f8d62
Anushka-shukla <shuklaannushka@gmail.com>
2023-08-07 00:50:04 +0530

    basic app setup done

---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Home=()=>{
+    return <div>
+        <h2>Find a match</h2>
+        <h4>Create a link</h4>
+        <input/>
+
+        <h4>Join a session</h4>
+        <input></input>
+    </div>
+}
+
+export default Home;
