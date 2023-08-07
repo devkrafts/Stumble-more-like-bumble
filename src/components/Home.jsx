0eb56f6ca8468ed718eb6db9d1f38e04cb0a0de0
Anushka-shukla <shuklaannushka@gmail.com>
2023-08-08 00:18:52 +0530

    added functionlity to create an uniqueId for joing session and added navigation for room

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import Room from "./Room";
+import { useNavigate } from "react-router";
 
-const Home=()=>{
+const Home = () => {
+
+    const handleSessionNavigation=()=>{
+        navigation('/session')
+    }
+
+    const navigation = useNavigate();
+
+    const [uniqueId, setUniqueId] = useState("");
+    const generateUniqueId = () => {
+        let key = "";
+        const length = 6;
+        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        const charactersLength = characters.length;
+        for (let counter = 0; counter < length; counter++) {
+            key += characters.charAt(Math.floor(Math.random() * charactersLength));
+
+        }
+        setUniqueId(key);
+    }
+
+    //console.log(generateUniqueId(10));
+
     return <div>
         <h2>Find a match</h2>
-        <h4>Create a link</h4>
-        <input/>
+        <button onClick={generateUniqueId}>Create a link</button>
+        <button>Copy</button>
+        <h4>{uniqueId}</h4>
+        <button onClick={handleSessionNavigation}>Start</button>
 
-        <h4>Join a session</h4>
+        <h4>Already have a link? Join the session directly..</h4>
         <input></input>
+        <button onClick={handleSessionNavigation}>Join</button>
     </div>
 }
 
