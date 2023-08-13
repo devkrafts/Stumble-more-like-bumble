57088d68f0fc357d6243b09b8d0a89af43a4544b
Anushka-shukla <shuklaannushka@gmail.com>
2023-08-13 19:22:38 +0530

    added sample data for restaurant

--- src/constants/restaurant.js
+++ src/constants/restaurant.js
@@ -1,30 +1,140 @@
 export const restaurant = [
-    {
-      "id": 0,
-      "image": "https://source.unsplash.com/random",
-      "name": "First Restuarant",
-      "ratings": "5",
-      "description": "Your description"
-    },
-    {
-      "id": 1,
-      "image": "https://source.unsplash.com/random",
-      "name": "Second Restuarant",
-      "ratings": "5",
-      "description": "Your description"
-    },
-    {
-      "id": 2,
-      "image": "https://source.unsplash.com/random",
-      "name": "third Restuarant",
-      "ratings": "4",
-      "description": "Your description"
-    },
-    {
-      "id": 3,
-      "image": "https://source.unsplash.com/random",
-      "name": "Fourth Restuarant",
-      "ratings": "5",
-      "description": "Your description"
-    }
-  ]
+  {
+    "id": 0,
+    "image": "http://www.just-eat.co.uk/restaurants-cn-chinese-cardiff/menu",
+    "address": "228 City Road",
+    "address line 2": "Cardiff",
+    "name": ".CN Chinese",
+    "rating": 5,
+    "phone": "+91 9043546532"
+  },
+  {
+
+    "id": 1,
+    "image": "http://www.just-eat.co.uk/restaurants-atthai-ss9/menu",
+    "address": "376 Rayleigh Road",
+    "address line 2": "Essex",
+    "name": "@ Thai",
+    "rating": 5.5,
+    "phone": "+91 9000546532"
+  },
+  {
+    "id": 2,
+    "image": "http://www.just-eat.co.uk/restaurants-atthairestaurant/menu",
+    "address": "30 Greyhound Road Hammersmith",
+    "address line 2": "London",
+    "name": "@ Thai Restaurant",
+    "rating": 4.5,
+    "phone": "+91 9000543432"
+  },
+  {
+    "id": 3,
+    "image": "http://www.just-eat.co.uk/restaurants-atthairestaurant/menu",
+    "address": "30 Greyhound Road Hammersmith",
+    "address line 2": "London",
+    "name": "@ Thai Restaurant",
+    "rating": 4.5,
+    "phone": "+91 9900543432"
+  },
+  {
+    "id": 4,
+    "image": "http://www.just-eat.co.uk/restaurants-indiancom-ch4/menu",
+    "address": "9 Broughton Hall Road",
+    "address line 2": "Broughton",
+    "name": "@Indian.com",
+    "rating": 6,
+    "phone": "+91 9900543432"
+  },
+  {
+    "id": 5,
+    "image": "http://www.just-eat.co.uk/restaurants-007takeaway-s65/menu",
+    "address": "6 Drummond Street",
+    "address line 2": "Rotherham",
+    "name": "007 Takeaway",
+    "rating": 6,
+    "phone": "+91 9900543432"
+  },
+  {
+    "id": 6,
+    "image": "http://www.just-eat.co.uk/restaurants-042-restaurant-e11/menu",
+    "address": "885 High Road Leytonstone",
+    "address line 2": "London",
+    "name": "042 Restaurant \u0026 Bar",
+    "rating": 3,
+    "phone": "+91 9960583932"
+  },
+  {
+    "id": 7,
+    "image": "http://www.just-eat.co.uk/restaurants-042-restaurant-e11/menu",
+    "address": "885 High Road Leytonstone",
+    "address line 2": "London",
+    "name": "042 Restaurant \u0026 Bar",
+    "rating": 3,
+    "phone": "+91 9907583932"
+  },
+  {
+    "id": 8,
+    "image": "http://www.just-eat.co.uk/restaurants-042-restaurant-e11/menu",
+    "address": "885 High Road Leytonstone",
+    "address line 2": "London",
+    "name": "042 Restaurant \u0026 Bar",
+    "rating": 3,
+    "phone": "+91 9901583932"
+  },
+  {
+    "id": 8,
+    "image": "http://www.just-eat.co.uk/restaurants-1-2-3-chinese-rowlands-gill/menu",
+    "address": "Unit 4 Spencer House",
+    "address line 2": "Swalwell",
+    "name": "1 2 3 Chinese",
+    "rating": 4.5,
+    "phone": "+91 9900513932"
+  },
+  {
+    "id": 10,
+    "image": "http://www.just-eat.co.uk/restaurants-1-2-3-chinese-rowlands-gill/menu",
+    "address": "Unit 4 Spencer House",
+    "address line 2": "Swalwell",
+    "name": "1 2 3 Chinese",
+    "rating": 4.5,
+    "phone": "+91 9900583932"
+  },
+  {
+    "id": 11,
+    "image": "http://www.just-eat.co.uk/restaurants-100-degrees-chinese-restaurant-pontypridd/menu",
+    "address": "67 Park Street",
+    "address line 2": "Treforest",
+    "name": "100 Degrees Chinese Restaurant",
+    "rating": 5,
+    "phone": "+91 9000583934"
+  },
+  {
+    "id": 12,
+    "image": "http://www.just-eat.co.uk/restaurants-100menu-wn1/menu",
+    "address": "50 Wallgate",
+    "address line 2": "Wigan",
+    "name": "100 Menu",
+    "rating": 5,
+    "phone": "+91 9950583932"
+  },
+  {
+    "id": 13,
+    "image": "http://www.just-eat.co.uk/restaurants-104-turkish-BBQ/menu",
+    "address": "104 Tanners Lane",
+    "address line 2": "Essex",
+    "name": "104 Turkish BBQ",
+    "rating": 5,
+    "phone": "+91 9430583932"
+  },
+  {
+    "id": 14,
+    "image": "http://www.just-eat.co.uk/restaurants-109restaurant-w2/menu",
+    "address": "109 Westbourne Park Road",
+    "address line 2": "London",
+    "name": "109 Ristorante",
+    "rating": 5,
+    "phone": "+91 9700583932"
+  }
+
+
+]
