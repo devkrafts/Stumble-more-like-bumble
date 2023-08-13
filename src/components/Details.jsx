db36a05414aa15b79ce7ae853ce15de2ad2dfa51
Anushka-shukla <shuklaannushka@gmail.com>
2023-08-13 23:35:49 +0530

    details page styling done

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,26 +1,56 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import { restaurant } from '../constants/restaurant';
+import { Star } from 'react-feather';
+import './styles/details.css';
+
 
 const Details = () => {
     const params = useParams()
     const [rest, setRest] = useState('')
 
-    useEffect(()=>{
+    useEffect(() => {
 
         console.log(params, restaurant)
-        setRest(restaurant.find((obj)=>`${obj.id}`===params.detailId)||{})
+        setRest(restaurant.find((obj) => `${obj.id}` === params.detailId) || {})
         console.log(rest)
     }, [params.detailId])
 
     return <div>
-        <img src={rest.image}/>
-        <h2> {rest.name}</h2>
-        <h3>5 km away</h3>
-        <h4>Cuisine: Mexican</h4>
-        <h5>Closed/open</h5>
-        <h3>{rest.ratings}</h3>
-        <button>Directions</button>
+        <div className="detail__header">Stumble</div>
+        <div className='card'>
+        
+            <div className='detail-card'>
+                <div className='img-container'>
+                    <img src={rest.image} />
+                </div>
+
+                <div className='layout'>
+                    <div className='header-name'>
+                        <h2>{rest.name}</h2>
+                    </div>
+                    <h3 className='rating'>5 
+
+                        {rest.rating &&
+                            Array.from({ length: Number(rest.rating) }).map(() => <Star width={20} fill={'var(--secondary)'} />)
+                        }
+
+                    </h3>
+                    <h3 className='address'>{rest.address}</h3>
+                    <h3 className='contact'>{rest.phone} </h3>
+
+                   
+
+                </div>
+                <h2 className='match-txt'>It's a match! </h2>
+
+            </div>
+        </div>
+
+
+
+
+
     </div>
 
 }
